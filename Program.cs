d5be726ed93cf2891c2f05b1f1f575481cc705de
Oliver El-Khoueiry <21OEl-Khoueiry@heckgrammar.co.uk>
2024-06-25 15:16:32 +0100

    hi

--- Program.cs
+++ Program.cs
@@ -13,20 +13,35 @@ namespace Y9_DEC_TO_BIN_SKELETON
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
             int myInt = 0; //just for testing single stepping
             string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
+            int myStringAsInt = Convert.ToInt32(myString);
 
-            //MAIN:  NUMBER CONVERSION PROGRAM
+            //Convert integer to string
+            string myIntAsString = Convert.ToString(myInt);
+            Console.WriteLine(myIntAsString);
+            Console.ReadLine();
 
-            //CODE GOES HERE
+            //Insert a string at the start of another string
+            string myCombinedString = myStringAsInt + myString;
+            Console.WriteLine(myCombinedString);
+
         }
 
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
-        {
-            //CODE GOES HERE
-
-            return result; //REMOVE THE RED LINE!
-        }
     }
-}
+} //watch me cast from string to int
+
+//            //MAIN:  NUMBER CONVERSION PROGRAM
+           
+
+//            //CODE GOES HERE
+//        }
+
+//        //static void means the function will not return a value so it does not need a data type 
+//        //...this function DOES return a value so the method must have a data type
+//        static string numberConversion(int number, int numberbase)
+//        {
+//            //CODE GOES HERE
+
+//            return result; //REMOVE THE RED LINE!
+//        }
+//    }
+//}
