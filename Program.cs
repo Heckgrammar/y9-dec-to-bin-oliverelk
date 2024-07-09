7e18260caa8c15f5a4c611d546550f520b7fad0a
Oliver El-Khoueiry <21OEl-Khoueiry@heckgrammar.co.uk>
2024-07-09 15:15:42 +0100

    new

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.Remoting.Services;
 using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,89 +36,104 @@ namespace Y9_DEC_TO_BIN_SKELETON
             Console.WriteLine("Enter number");
             int userNum = Convert.ToInt32(Console.ReadLine());
             int remainder = userNum % 2;
-              
+            int divisionNum = 2;
+            int quotient = userNum / 2;
             int i = 0;
-            string remainderstring = Convert.ToString(remainder);
-            string[] fullBin = { remainderstring, remainderstring, remainderstring, remainderstring, remainderstring };
 
-            while (i == 0)
+            if (remainder > -1 && quotient > -1)
             {
-                if (remainder == 1)
-                {
-                    while (remainder == 1) 
-                    remainder = remainder % 2;
-                    Console.WriteLine(remainder);
-                    i++;
-
-                    //if (remainder == 0) ;
-                    //Console.WriteLine(remainder);
-                    //i++;
-                }
-
-                //if (remainder == 0) ;
-                //{
-                //    Console.WriteLine(remainder);
-        
-                //}
-
-                //int i = 1;
-
-                //if (remainder < 1)
-                //{
-                //    Console.WriteLine();
-                //}
-                //else
-                //{
-                //    while (remainder > 0)
-
-                //        if (remainder == 0)
-                //        {
-                //            string remainderstring = Convert.ToString(remainder);
-                //            string[] binaryNum = { remainderstring, remainderstring, remainderstring, remainderstring, remainderstring, remainderstring, remainderstring };
-                //            Console.WriteLine(binaryNum);
-                //            i++;
-                //        }
-                //        else while (i > 0)
-                {
-
-
-
-                }
-
-
-
-
-
-                //Console.WriteLine(remainder);
-                //string stringRemainder = Convert.ToString(remainder);
-                //string userStringNum = Convert.ToString(userNum);
-                ////userStringNum[i] = Convert.ToString(remainder);
-                //i = i + 1;
-                //int binaryNum = remaindercurrent;
-                //Console.WriteLine(binaryNum);
-                //Console.ReadLine();
-
-
+                while (i == 0)
+                    Console.WriteLine(userNum % 2);
+                string quotientstring = Convert.ToString(quotient);
+                if (remainder == 1 && quotient == 0) ;
+                i++;
+                Console.WriteLine(quotientstring);
+                Console.WriteLine("Done");
             }
+
+
+            
+
         }
     }
 }
 
-//} //watch me cast from string to int
 
-//            //MAIN:  NUMBER CONVERSION PROGRAM
-           
 
-//            //CODE GOES HERE
-//        }
 
-//        //static void means the function will not return a value so it does not need a data type 
-//        //...this function DOES return a value so the method must have a data type
-//        static string numberConversion(int number, int numberbase)
+
+
+
+
+//Console.WriteLine("Enter number");
+//int userNum = Convert.ToInt32(Console.ReadLine());
+//int remainder = userNum % 2;
+
+//int i = 0;
+//string remainderstring = Convert.ToString(remainder);
+//string[] fullBin = { remainderstring, remainderstring, remainderstring, remainderstring, remainderstring };
+
+//while (i == 0)
 //{
-//    //CODE GOES HERE
+//    if (remainder == 1)
+//    {
+//        while (remainder == 1)
+//            remainder = remainder % 2;
+//        Console.WriteLine(remainder);
+//        i++;
 
-//    return result; //REMOVE THE RED LINE!
-//}
+//        //if (remainder == 0) ;
+//        //Console.WriteLine(remainder);
+//        //i++;
 //    }
+
+//                //if (remainder == 0) ;
+//                //{
+//                //    Console.WriteLine(remainder);
+
 //}
+
+//int i = 1;
+
+//if (remainder < 1)
+//{
+//    Console.WriteLine();
+//}
+//else
+//{
+//    while (remainder > 0)
+
+//        if (remainder == 0)
+//        {
+//            string remainderstring = Convert.ToString(remainder);
+//            string[] binaryNum = { remainderstring, remainderstring, remainderstring, remainderstring, remainderstring, remainderstring, remainderstring };
+//            Console.WriteLine(binaryNum);
+//            i++;
+//        }
+//        else while (i > 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
